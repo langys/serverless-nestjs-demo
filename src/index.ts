d3306ef8cb60d34a3ee8ef962125b48334f026ee
郎永山 <evi1b0dy@langyongshandeMacBook-Pro.local>
2020-03-15 17:02:48 +0800

    improvement: reduce serverless entry index code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Server } from 'http';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as express from 'express';
-import { createServer } from 'tencent-serverless-http';
 
-export const bootstrapServer = async (): Promise<Server> => {
+export const createApp = async (): Promise<express.Express> => {
     const expressApp = express();
     const adapter = new ExpressAdapter(expressApp);
     const app = await NestFactory.create(AppModule, adapter);
     app.enableCors();
     await app.init();
-    return createServer(expressApp)
+    return expressApp
 }
